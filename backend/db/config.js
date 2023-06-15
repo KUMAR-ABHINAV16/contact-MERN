459a4547ef6149c5850cb6df12a3b5d811d2dc18
KUMAR-ABHINAV16 <97382659+KUMAR-ABHINAV16@users.noreply.github.com>
2023-06-15 10:27:16 +0530

    new-project

---
+++ backend/db/config.js
@@ -0,0 +1,19 @@
+const mongoose= require('mongoose')
+ 
+// const uri = 'mongodb+srv://yadav:Yadav123@cluster0.y0pvkqz.mongodb.net/contactlist?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://abhinav69596959:123321@cluster0.dg1z1mt.mongodb.net/contactlist?retryWrites=true&w=majority';
+mongoose.set('strictQuery', false);
+
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+    
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+    // Your code here
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB Atlas:', error);
+  });
